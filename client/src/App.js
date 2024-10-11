2cda284dcbb33080aacc4638600d9fb976558d5d
michal.fingerut <michal.fingerut@infinitylabs.co.il>
2024-10-11 22:48:58 +0300

    /

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,31 @@ import KeeperMainPage from './components/KeeperMainPage';
 import LogInPage from './components/LogInPage';
 import './styles/App.css';
 import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [isLogged, setIsLogged] = useState(
     JSON.parse(localStorage.getItem('isLogged')),
   );
   const uuid = localStorage.getItem('uuid');
+
+  // return (
+  //   <>
+  //     <BrowserRouter basename="/keeper">
+  //       <Routes>
+  //         <Route
+  //           path="/"
+  //           element={<KeeperMainPage uuid={uuid} setIsLogged={setIsLogged} />}
+  //         ></Route>
+
+  //         <Route
+  //           path="/login"
+  //           element={<LogInPage setIsLogged={setIsLogged} />}
+  //         ></Route>
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </>
+  // );
 
   if (isLogged) {
     //TODO: routing
