17b0dd251aa8a70e5f9a998cc0e0e3c052889832
michal.fingerut <michal.fingerut@infinitylabs.co.il>
2024-10-31 15:40:07 +0200

    socket listeners template

---
+++ client/src/services/SocketEventHandlers.js
@@ -0,0 +1,45 @@
+//TODO:
+
+const socketEvents = [
+  {
+    name: 'delete group',
+    action: function (store) {
+      return (data) => {};
+    },
+  },
+  {
+    name: 'add member to group',
+    action: function (store) {
+      return (data) => {};
+    },
+  },
+  {
+    name: 'delete member from group',
+    action: function (store) {
+      return (data) => {};
+    },
+  },
+  {
+    name: 'post note',
+    action: function (store) {
+      return (data) => {};
+    },
+  },
+  {
+    name: 'delete note',
+    action: function (store) {
+      return (data) => {};
+    },
+  },
+  {
+    name: 'put note',
+    action: function (store) {
+      return (data) => {};
+    },
+  },
+];
+
+function initSocketEvents(store) {
+  // const socket = getSocket();
+  // socketEvents.forEach(({ name, action }) => socket.on(name, action(store)));
+}
