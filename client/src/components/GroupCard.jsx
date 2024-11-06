0e768e41b6206fe2a3ff31260b9ce1bef9df9f8c
michal.fingerut <michal.fingerut@infinitylabs.co.il>
2024-11-06 18:07:19 +0200

    add comment

--- client/src/components/GroupCard.jsx
+++ client/src/components/GroupCard.jsx
@@ -1,8 +1,10 @@
+//External modules
+import Tooltip from '@mui/material/Tooltip';
+
 // Internal modules
 import { CardStyle, CenteredHeader } from '../styles/styles';
 import KeeperMenu from './Menu';
 import backApi from '../services/backApi';
-import Tooltip from '@mui/material/Tooltip';
 
 function GroupCard(props) {
   ///////////////////// props /////////////////////
